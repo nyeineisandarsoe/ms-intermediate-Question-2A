72d9b41e7ed2f1791f5057559d7998ebdb98bb86
Sandar <Nyein_Ei_Sandar_Soe_from.tp@simtech.a-star.edu.sg>
2022-05-10 15:48:54 +0800

    first commit

---
+++ Producer/Controllers/TokenController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Producer.Models;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
+
+namespace Producer.Controllers
+{
+    [ApiController]
+    [Route("api/user/[controller]")]
+    public class TokenController : ControllerBase
+    {
+
+        [HttpPost]
+        async public void Post([FromBody] User user)
+        {
+            //get token
+            string token = await getToken();
+
+            var factory = new ConnectionFactory()
+            {
+                //HostName = "localhost" , 
+                //Port = 30724
+                HostName = Environment.GetEnvironmentVariable("RABBITMQ_HOST"),
+                Port = Convert.ToInt32(Environment.GetEnvironmentVariable("RABBITMQ_PORT"))
+            };
+
+            Console.WriteLine(factory.HostName + ":" + factory.Port);
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "TaskQueue",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                string message = token;
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "TaskQueue",
+                                     basicProperties: null,
+                                     body: body);
+            }
+        }
+
+        private async Task<string> getToken()
+        {
+            var values = new Dictionary<string, string>
+              {
+                  { "email", "eve.holt@reqres.in" },
+                  { "password", "pistol" },
+                  { "task", "Any Task" }
+              };
+
+            var content = new FormUrlEncodedContent(values);
+
+            var token = "";
+            var url = "https://reqres.in/api/register";
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(url);
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = await client.PostAsync(url, content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonString = await response.Content.ReadAsStringAsync();
+                var tokenObj = JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonString);
+
+                if (tokenObj != null)
+                {
+                    token = tokenObj["token"];
+                }
+            }
+
+            return token;
+        }
+    }
+
+}
---
+++ Producer/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Producer.Models
+{
+    public class User
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Task { get; set; }
+        public string Token { get; set; }
+    }
+}
